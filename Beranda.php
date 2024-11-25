80a37cedd68f7163576edc7bf6f733706808bfcf
Ihsan051 <111128372+Ihsan051@users.noreply.github.com>
2024-11-25 18:34:04 +0800

    memperbaiki tampilan card di beranda

--- Beranda.php
+++ Beranda.php
@@ -22,6 +22,7 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kamus Pribadi</title>
     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -53,44 +54,58 @@ $result = $conn->query($sql);
 <body>
     <?php include "navbar.php"; ?>
     <!-- Main Content -->
-                <section class="container my-5">
-        <div class="row">
-            <div class="col-md-3 p-2">
-                <div id="data-container" class="d-flex flex-wrap">
-                <?php 
-                    if ($result->num_rows > 0) {
-                        // menampilkan data 
-                        while ($row = $result->fetch_assoc()){
-                        echo "<div class='card d-inline-block' style= width: 10rem;' >";
-                        echo  "<img src='". $row['image_path'] ."' class='card-img-top w-100' >";
-                            echo "<div class='card-body bg-dark text-light pb-0'>";
-                            echo "<table class='table table-dark table-borderless w-25 lh-1'>";
-                            echo "<tr>";
-                            echo "<td>Id</td>";
-                            echo "<td>:" . $row["id"] ."</td>";
-                            echo "</tr>";
-                            echo "<tr>";
-                            echo "<td>Kata</td>";
-                            echo "<td> :". $row["word"] ."</td>";
-                            echo "</tr>";
-                            echo "<tr>";
-                            echo "<td>Definisi</td>";
-                            echo "<td>:" . $row["definition"] ."</td>";
-                            echo "</tr>";
-                            echo "<tr>";
-                            echo "<td>Contoh </td>";
-                            echo "<td>:" . $row["example"] ."</td>";
-                            echo "</tr>";
-                            echo "</table>";
-                            echo "</div>";
-                        }
-                    }
-                    
-                    ?>
+<div class="container-fluid py-5">
+    <div class="container text-center">
+        <h3> Daftar Kamus</h3>
+
+        <div class="row mt-5">
+            <?php 
+            if ($result->num_rows > 0) {
+                // menampilkan data
+            while ($row = $result->fetch_assoc()) { ?>
+            <div class="col-sm-6 col-md-4">
+                <div class="card" >
+                    <img src="<?php  echo $row['image_path']; ?>" class="card-img-top" alt="...">
+                    <div class="card-body bg-dark ">
+                    <table class="table table-dark text-start table-borderless">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col"> : <?php echo $row['id']; ?></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Kata</th>
+                                <td > : <?php echo $row['word']; ?></td>
+                                <td colspan="4"></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Definisi</th>
+                                <td colspan="4"> : <?php echo $row['definition']; ?></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Contoh</th>
+                                <td colspan="4"> : <?php echo $row['example']; ?></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
-          </div>
-        </div>
-    </section>
+            </div>
+            <?php }
+            }?>
+    </div>
+</div>
+
     <!-- Tombol Tambah Data -->
     <section class="container my-5 text-center">
         <a href="inputdata.php" class="btn btn-success btn-lg">Tambah Data Baru</a>
